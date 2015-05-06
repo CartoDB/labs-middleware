6697818042fcd7d10630606deb1e8c69fe6f9782
Dani Carrion <daniel@cartodb.com>
2015-05-06 22:59:49 +0200

    dokku support

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn middleware:app --log-file=-
--- middleware.py
+++ middleware.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python
 import ConfigParser
+import os
 import requests
 import ssl
 from flask import Flask, request, session, redirect, url_for, render_template, flash, Response, jsonify
 
 
+class Config(object):
+    def __init__(self, config_file_name):
+        self.config_parser = ConfigParser.RawConfigParser()
+        self.config_parser.read(config_file_name)
+
+    def get(self, section, key):
+        try:
+            return self.config_parser.get(section, key)
+        except ConfigParser.NoSectionError:
+            return os.environ.get("%s_%s" % (section.upper(), key.upper), None)
+
+config = Config("middleware.conf")
+
+
 app = Flask(__name__)
-
-config = ConfigParser.RawConfigParser()
-config.read("middleware.conf")
-
-app.secret_key = config.get('platform', 'secret_key')
 
 if config.get('platform', 'debug'):
     app.debug = True
+
+app.secret_key = config.get('platform', 'secret_key')
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -29,20 +41,20 @@ def index():
 
     return render_template('index.html', user=session['username'] if "username" in session else None)
 
-@app.route('/logout')
+@app.route('/logout/')
 def logout():
     session.pop('username', None)
     return redirect(url_for('index'))
 
 
-@app.route("/sql/provinces/")
+@app.route(config.get('platform', 'sql_endpoint'))
 def sql_provinces():
     if "username" not in session:
         return redirect(url_for('index'))
 
     # Call CartoDB's SQL API
     params = {
-        "q": "select cartodb_id, ccaa, codigo, nombre from provincias",
+        "q": config.get('map', 'sql'),
         "api_key": config.get('cartodb', 'api_key'),
     }
     r = requests.get(config.get('cartodb', 'sql_endpoint'), params)
@@ -50,7 +62,7 @@ def sql_provinces():
     return Response(r.content, mimetype='application/json')
 
 
-@app.route("/map/provinces/")
+@app.route(config.get('platform', 'map_endpoint'))
 def map_provinces():
     return jsonify({"token": config.get('map', 'token'),
                     "username": config.get('cartodb', 'username'),
